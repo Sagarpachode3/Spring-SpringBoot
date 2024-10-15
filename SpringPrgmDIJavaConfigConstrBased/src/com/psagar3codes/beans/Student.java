2ff97479f76162c18383454df58c4d689540cb0e
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-10-15 23:25:27 +0530

    Setter based and Constructor Based DI using Java config annotations.

---
+++ SpringPrgmDIJavaConfigConstrBased/src/com/psagar3codes/beans/Student.java
@@ -0,0 +1,22 @@
+package com.psagar3codes.beans;
+
+public class Student {
+
+	private int rollNo;
+	private String name;
+	private Address address;
+
+	public Student(int rollNo, String name, Address address) {
+		super();
+		this.rollNo = rollNo;
+		this.name = name;
+		this.address = address;
+	}
+
+	public void display() {
+		System.out.println("Roll No :" + rollNo);
+		System.out.println("Name :" + name);
+		System.out.println("Address :" + address);
+
+	}
+}
